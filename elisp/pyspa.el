9c93a2b7a12467f64e6449d8637b783169ff8495
mopemope <yutaka.matsubara@gmail.com>
2020-05-10 02:52:50 +0900

    add elisp

---
+++ elisp/pyspa.el
@@ -0,0 +1,15 @@
+;;; pyspa.el --- This package contains a variety of features -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright (C) 2020 Yutaka Matsubara
+
+;; Author: Yutaka Matsubara (yutaka.matsubara@gmail.com)
+;; Homepage: https://github.com/pyspa/emacs-is-not-dead
+;; Package-Version: 0.0.1
+;; Package-Requires: ((s "1.12.0") (dash "2.17.0") (dash-functional "1.2.0") (emacs "24"))
+
+(require 'cl-lib)
+(require 'pcase)
+(require 'thingatpt)
+(require 's)
+(require 'dash)
+(require 'dash-functional)
